4155a692ea2d4987caec3baa4cf954a42f67687b
unknown <shodmonovjavohir56@gmail.com>
2024-06-11 10:18:21 +0500

    first

---
+++ script.js
@@ -0,0 +1,61 @@
+// let object = new Object()
+// console.log(object);
+
+// const obj = {
+//     name: 'Anna',
+//     age: 18,
+//     speaking: true,
+//     arr: ['olma', 'qulupnay']
+
+// }
+// delete // object keyi bo'yicha o'chiradi
+// let obj = {}
+// obj['name'] = 'Adam'
+// console.log(obj);
+
+
+
+
+// let user = {
+//     name: 'Anna',
+//     surname: 'Hathaway',
+//     tglink: "annah",
+//     Hisoblagich: function() {alert('FrontEnd')}
+// }
+// user.Hisoblagich()
+
+// let user ={
+//     '?': 'Qarshi',
+//     if: 'Uzbekistan',
+//     return: 'Asia',
+// }
+
+// console.log("?" in user);
+
+// let user = {
+//     name: "John",
+//     age: 30,
+//     isAdmin: true
+// };
+
+// for(el in user){
+//     console.log(user[el]);
+// }
+// let codes = {
+//     "49": "Германия",
+//     "41": "Швейцария",
+//     "44": "Великобритания",
+//     "1": "США"
+// };
+
+// for(key in codes){
+//     alert(codes[key])
+// }
+
+let salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+// 'hello' ==> "olleh"
+console.log('hello'.split('').reverse().join(''));
